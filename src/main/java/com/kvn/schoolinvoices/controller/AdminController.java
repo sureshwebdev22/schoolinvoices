23aabc8d4d6332d81aad992c15b34bac0cccecb9
suresh <sureshwebdev22@gmail.com>
2026-06-29 14:13:16 +0530

    initial

--- src/main/java/com/kvn/schoolinvoices/controller/AdminController.java
+++ src/main/java/com/kvn/schoolinvoices/controller/AdminController.java
@@ -1,4 +1,4 @@
-package com.kvn.schoolinvoices;
+package com.kvn.schoolinvoices.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
