1ac38ec03cd06b47e7e0694387f63582f3704b43
suresh <sureshwebdev22@gmail.com>
2026-07-26 18:03:12 +0530

    pipeline

--- src/scripts/after_install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-chmod +x /home/ec2-user/app1/*.jar
--- src/scripts/start_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-JAR=$(ls /home/ec2-user/app1/*.jar | head -1)
-
-cp "$JAR" /home/ec2-user/app1/app.jar
-
-sudo systemctl restart springboot
-
-chmod +x scripts/*.sh
